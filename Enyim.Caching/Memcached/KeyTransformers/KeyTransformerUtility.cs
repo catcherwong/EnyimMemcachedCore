841dac43747189641e6b86e6cd3277e1df6461ad
Catcher Wong <catcher_hwq@outlook.com>
2018-12-29 22:14:41 +0800

    Revert "Rename TransformSHA1() to ToSHA1Hash()"
    This reverts commit d7582fbe2de7fca961a3d347bba6a80b1a76a684.

--- Enyim.Caching/Memcached/KeyTransformers/KeyTransformerUtility.cs
+++ Enyim.Caching/Memcached/KeyTransformers/KeyTransformerUtility.cs
@@ -9,7 +9,7 @@ namespace Enyim.Caching.Memcached.KeyTransformers
 {
     public static class KeyTransformerUtility
     {
-        public static string ToSHA1Hash(string key)
+        public static string TransformSHA1(string key)
         {
             return new SHA1KeyTransformer().Transform(key);
         }
