28dc38886f88f192019a66d48e128dc2c134f254
Catcher Wong <catcher_hwq@outlook.com>
2018-12-29 22:14:41 +0800

    Revert "Update build.sh and run-test.sh"
    This reverts commit 0ba23e0c7e8fa8a4fae0bd9ee5a6e36b7e0ceb7d.

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-dotnet test Enyim.Caching.Tests/Enyim.Caching.Tests.csproj -c Release
-dotnet test MemcachedTest/MemcachedTest.csproj -c Release
+dotnet test Enyim.Caching.Tests/Enyim.Caching.Tests.csproj
