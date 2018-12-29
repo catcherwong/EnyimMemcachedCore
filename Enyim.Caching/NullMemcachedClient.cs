4c424d26b8d5c1990a17fc08d22d0a805b62da7c
Catcher Wong <catcher_hwq@outlook.com>
2018-12-29 22:14:41 +0800

    Revert "Add Set and SetAsync api"
    This reverts commit 7163fed2a00537354e1a31d2ac0e93841ab4be94.

--- Enyim.Caching/NullMemcachedClient.cs
+++ Enyim.Caching/NullMemcachedClient.cs
@@ -230,19 +230,8 @@ namespace Enyim.Caching
         {
         }
 
-        public Task AddAsync(string key, object value, int cacheSeconds)
+        public async Task AddAsync(string key, object value, int cacheSeconds)
         {
-            return Task.CompletedTask;
-        }
-
-        public void Set(string key, object value, int cacheSeconds)
-        {
-
-        }
-
-        public Task SetAsync(string key, object value, int cacheSeconds)
-        {
-            return Task.CompletedTask;
         }
     }
 }
